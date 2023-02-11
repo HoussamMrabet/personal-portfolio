b8e384a2a9189e97efd4494f8801fb62515f9bd1
Houssam Mrabet <dodslim100@gmail.com>
2023-02-11 20:20:06 +0100

    style navbar elements

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import './Navbar.scss';
+import { images } from "../../constants";
+import "./Navbar.scss";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const navElements = [
+    "Home",
+    "About",
+    "Work",
+    "Skills",
+    "Testimonial",
+    "Contact",
+  ];
 
-export default Navbar
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navabar-links">
+        {navElements.map((element) => (
+          <li className="app__flex p-text" key={`link-${element}`}>
+            <div />
+            <a href={`#${element}`}>{element}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
