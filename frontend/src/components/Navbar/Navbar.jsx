f75404d14e3efd039f9369a7d6cbd7309c5dc062
Houssam Mrabet <dodslim100@gmail.com>
2023-02-11 21:44:05 +0100

    make the navbar responsive

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 import { images } from "../../constants";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   const navElements = [
     "Home",
     "About",
@@ -18,7 +22,7 @@ const Navbar = () => {
       <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
-      <ul className="app__navabar-links">
+      <ul className="app__navbar-links">
         {navElements.map((element) => (
           <li className="app__flex p-text" key={`link-${element}`}>
             <div />
@@ -26,6 +30,26 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+          transition={{ duration: 0.85, ease: "easeOut" }}
+          whileInView={{ x: [300, 0] }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {navElements.map((element) => (
+                <li key={element}>
+                  <a href={`#${element}`} onClick={() => setToggle(false)}>{element}</a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
