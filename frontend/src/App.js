7189f014913cdb8ba15b6f824b187735a706c2bd
Houssam Mrabet <dodslim100@gmail.com>
2023-02-11 18:11:51 +0100

    set the frontend environment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import { Header, Footer, About, Skills, Testimonial, Work } from "./container";
+import { Navbar} from './components';
+
+import './App.scss';
+
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Work />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
