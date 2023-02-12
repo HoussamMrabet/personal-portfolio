a59fc1ba509167356e3209a8642c341b906a97ea
Houssam Mrabet <dodslim100@gmail.com>
2023-02-13 00:49:05 +0100

    last touches

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook, FaLinkedin } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
-      <div>
+      <a
+        href="https://www.linkedin.com/in/houssam-mrabet-6b758a176/"
+        target="_blank"
+      >
+        <FaLinkedin />
+      </a>
+      <a href="https://twitter.com/MrabetHoussam" target="_blank">
         <BsTwitter />
-      </div>
-      <div>
+      </a>
+      <a href="https://www.instagram.com/houssam_mrabet_/" target="_blank">
         <BsInstagram />
-      </div>
-      <div>
+      </a>
+      <a href="https://www.facebook.com/houbet" target="_blank">
         <FaFacebook />
-      </div>
+      </a>
     </div>
   );
 };
