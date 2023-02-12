6b442d7b8f98055b530525ea028b6b7b11b144ce
Houssam Mrabet <dodslim100@gmail.com>
2023-02-13 00:17:53 +0100

    set footer

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -34,6 +34,7 @@ import bolt from "../assets/bolt.png";
 import nb from "../assets/nb.png";
 import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
+import gmail from "../assets/gmail.png";
 
 const images = {
   email,
@@ -69,6 +70,7 @@ const images = {
   nb,
   skype,
   spotify,
+  gmail,
 };
 
 export default images;
