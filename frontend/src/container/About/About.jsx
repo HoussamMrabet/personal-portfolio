c8247bf4bd0bb45a261980b1105e97d8349c3b5f
Houssam Mrabet <dodslim100@gmail.com>
2023-02-12 22:50:50 +0100

    decorate sections

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
-import { AppWrap } from "../../wrapper";
-import { images } from "./../../constants";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import {urlFor, client} from '../../client';
 import "./About.scss";
 
@@ -47,4 +46,4 @@ const About = () => {
   );
 };
 
-export default AppWrap(About, 'about');
+export default AppWrap(MotionWrap(About, 'app__about'), 'about', "app__whitebg");
